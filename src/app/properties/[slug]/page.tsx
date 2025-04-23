be04d00989c04645f2629eb9afcdd913920b6539
rusdifz <fauzanrusdi20@gmail.com>
2025-04-24 00:11:55 +0700

    test: test

--- src/app/properties/[slug]/page.tsx
+++ src/app/properties/[slug]/page.tsx
@@ -4,20 +4,11 @@ import { useParams } from 'next/navigation';
 import PropertyDetail from '@/components/use/property-detail';
 import Wrapper from '@/layouts/Wrapper';
 
-// export const metadata = {
-//   title: "Listing Details One Homy - Real Estate React Next js Template",
-// };
-
 const Index = () => {
   const params = useParams();
   const { slug } = params; // Menangkap id dari URL
-  // console.log('slug', slug);
 
-  return (
-    <Wrapper>
-      <PropertyDetail slug={slug} />
-    </Wrapper>
-  );
+  return <Wrapper>{/* <PropertyDetail slug={slug} /> */}</Wrapper>;
 };
 
 export default Index;
