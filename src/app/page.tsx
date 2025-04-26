d05097e01bb6d6f69d2f9ecf17ac411c0e01d547
rusdifz <fauzanrusdi20@gmail.com>
2025-04-26 22:05:46 +0700

    test: test fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Wrapper from '@/layouts/Wrapper';
-// import Home from '@/layouts/homepage';
+import Home from '@/layouts/homepage';
 
 const index = () => {
-  return <Wrapper>{/* <Home /> */}</Wrapper>;
+  return (
+    <Wrapper>
+      <Home />
+    </Wrapper>
+  );
 };
 
 export default index;
