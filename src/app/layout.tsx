6c4a1eaedde8c52cee061d5af963a92837fab4b3
rusdifz <rusdi@femaledaily.com>
2025-08-27 10:11:42 +0700

    feat: add script ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,8 +46,10 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,500&display=swap"
         />
+        <script> (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-KW8V6BHQ'); </script>
       </head>
       <body suppressHydrationWarning={true}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KW8V6BHQ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <div className="main-page-wrapper">
           <Provider store={store}>
             <Header />
