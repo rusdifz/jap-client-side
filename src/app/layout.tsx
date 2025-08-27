bf48c8742286a1a98eb5e668baec9e675e390f7b
rusdifz <rusdi@femaledaily.com>
2025-08-28 05:31:40 +0700

    feat: add tag analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,16 +21,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={isDev}>
       <head>
-        {/* GTM Script */}
-        <Script id="gtm-script" strategy="afterInteractive">
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-74ES17K0X0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KW8V6BHQ');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-74ES17K0X0');
           `}
         </Script>
+
         <meta
           name="keywords"
           content="workspace jabodetabek, jll, regus, property, office, office space, jabodetabek,coworking space jakarta"
